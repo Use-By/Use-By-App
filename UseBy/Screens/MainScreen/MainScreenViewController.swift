60279e058ca2a408981cb5a369514caf0a19c7c1
Svoykina <n-svoykina@yandex-team.ru>
2020-10-26 11:53:46 +0300

    Добавила кнопку Log Out

--- UseBy/Screens/MainScreen/MainScreenViewController.swift
+++ UseBy/Screens/MainScreen/MainScreenViewController.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 import UIKit
+import GoogleSignIn
 
 class MainScreenViewController: UIViewController {
+    struct UIConstants {
+        static let logOutButtonPadding: CGFloat = 20
+    }
+
     private let emptyScreenLabel = UILabel()
+    private let logOutButton = MainButton(
+        text: "log-out".localized, theme: .pseudo
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +25,7 @@ class MainScreenViewController: UIViewController {
         view.backgroundColor = Colors.mainBGColor
 
         configureEmptyScreenLabel()
-
+        configureLogOutButton()
     }
 
     func configureEmptyScreenLabel() {
@@ -28,6 +36,24 @@ class MainScreenViewController: UIViewController {
         view.addSubview(emptyScreenLabel)
         emptyScreenLabel.snp.makeConstraints { (make) -> Void in
             make.center.equalTo(view)
+            make.width.equalTo(view)
         }
+    }
+
+    func configureLogOutButton() {
+        view.addSubview(logOutButton)
+        logOutButton.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(emptyScreenLabel.snp.bottom).offset(UIConstants.logOutButtonPadding)
+            make.centerX.equalTo(view)
+            make.height.equalTo(MainButton.buttonHeight)
+            make.width.equalTo(view).inset(UIConstants.logOutButtonPadding)
+        }
+        logOutButton.addTarget(self, action: #selector(didTapLogOutButton), for: .touchUpInside)
+    }
+
+    @objc
+    private func didTapLogOutButton() {
+        GIDSignIn.sharedInstance()?.signOut()
+        self.view.window?.rootViewController = MainAuthViewController()
     }
 }

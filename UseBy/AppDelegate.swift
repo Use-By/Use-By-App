5098c5d4875f3f8d374a746e96351bafd3e44db4
fillinmar <marina_novikova_2001@mail.ru>
2020-11-15 16:52:00 +0300

    profile and alerts

--- UseBy/AppDelegate.swift
+++ UseBy/AppDelegate.swift
@@ -15,8 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         window = UIWindow(frame: UIScreen.main.bounds)
-        //window!.rootViewController = MainAuthViewController()
-        window!.rootViewController = ProfileViewController()
+        window!.rootViewController = MainAuthViewController()
         window!.makeKeyAndVisible()
 
         return true

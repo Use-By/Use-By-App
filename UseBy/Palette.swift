49b826d6e0c7d0a53cbf83d0f3b9c6d95add0f18
Svoykina <n-svoykina@yandex-team.ru>
2020-12-29 00:45:54 +0300

    Dark mode support

--- UseBy/Palette.swift
+++ UseBy/Palette.swift
@@ -10,12 +10,12 @@ import UIKit
 
 struct Colors {
     // Цвета текстов
-    static let mainTextColor = UIColor.black
+    static let mainTextColor = UIColor(named: "mainTextColor") ?? UIColor.black
     static let inversedTextColor = UIColor.white
-    static let secondaryTextColor = UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.6)
+    static let secondaryTextColor = UIColor(named: "secondaryTextColor") ?? UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.6)
 
     // Цвета фонов
-    static let mainBGColor = UIColor.white
+    static let mainBGColor = UIColor(named: "mainBGColor") ?? UIColor.white
     static let mainActionBGColor = UIColor(red: 254/255, green: 169/255, blue: 155/255, alpha: 0.1)
     static let socialBGColor = UIColor(red: 66/255, green: 103/255, blue: 177/255, alpha: 1.0)
     static let secondaryActionBGColor = UIColor(red: 64/255, green: 188/255, blue: 216/255, alpha: 0.1)
@@ -45,7 +45,7 @@ struct Colors {
     static let defaultIconColor = UIColor(red: 79/255, green: 79/255, blue: 79/255, alpha: 1.0)
     static let inversedIconColor = UIColor.white
     static let actionIconColor = UIColor(red: 255/255, green: 97/255, blue: 128/255, alpha: 1.0)
-    static let actionIconShadowColor = UIColor(red: 255/255, green: 204/255, blue: 215/255, alpha: 1.0)
+    static let actionIconShadowColor = UIColor(red: 255/255, green: 97/255, blue: 128/255, alpha: 0.6)
     static let secondaryIconColor = UIColor(red: 189/255, green: 189/255, blue: 189/255, alpha: 1.0)
     static let inactiveIconColor = UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1.0)
 
